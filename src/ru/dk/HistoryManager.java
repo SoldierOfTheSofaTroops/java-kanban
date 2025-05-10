1db4f95167a0a745e23bc1c7c8ff6bcf06765d41
deniskorovko <dkcreator@gmail.com>
2025-05-10 10:31:27 +0300

    Add void remove(int id) to interface HistoryManager. Implements void remove(int id) in InMemoryHistoryManager

--- src/ru/dk/HistoryManager.java
+++ src/ru/dk/HistoryManager.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 public interface HistoryManager {
 
-    public void add(Task task);
+    void add(Task task);
 
-    public List<Task> getHistory();
+    void remove(int id);
+
+    List<Task> getHistory();
 }
--- src/ru/dk/InMemoryHistoryManager.java
+++ src/ru/dk/InMemoryHistoryManager.java
@@ -1,11 +1,18 @@
 package ru.dk;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager{
     private ArrayList<Task> watchedTasks = new ArrayList<>();
+    private HashMap<Integer, Task> nodes = new HashMap<>();
+    private HashMap<Integer, Task> newWatchedTasks = new HashMap<>();
 
+    /**
+     *<p>This method adds all types of tasks to history</p>
+     *<p>Deep copying is implemented in the method</p>
+     * @param task All types of tasks (see the {@link ru.dk.Task}
+     * {@link ru.dk.Subtask} {@link ru.dk.Epic} classes)
+     * */
     @Override
     public void add(Task task) {
         if (task != null){
@@ -22,7 +29,16 @@ public class InMemoryHistoryManager implements HistoryManager{
     }
 
     @Override
+    public void remove(int id) {
+        newWatchedTasks.remove(id);
+    }
+
+    public void removeNode(Node<Task> node) {
+
+    }
+
+    @Override
     public List<Task> getHistory() {
-        return watchedTasks;
+        return new ArrayList<>(nodes.values());
     }
 }
